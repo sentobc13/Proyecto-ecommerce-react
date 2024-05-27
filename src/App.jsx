1b2af6efcef16fd806362aa393cd7d7936109517
sentobc13 <sentoberlanga@gmail.com>
2024-05-27 15:09:07 +0200

    routes App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Login from './components/Login/Login'
+import Products from './components/Products/Products'
+import Profile from './components/Profile/Profile'
+import Cart from './components/Cart/Cart'
+import Register from './components/Register/Register'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <>
-<BrowserRouter>
-<Header/>
-<Routes>
-  {/* <Route path='' element={}/> */}
-</Routes>
-</BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/products' element={<Products/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
