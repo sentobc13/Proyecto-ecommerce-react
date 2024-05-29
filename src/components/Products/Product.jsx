5541d75d83a50de11cf061d8ed09c1c6915a3fa7
sentobc13 <sentoberlanga@gmail.com>
2024-05-29 16:51:12 +0200

    arreglos cards products

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
-import { Button, Card } from 'antd'
-import Meta from 'antd/es/card/Meta'
+import React from 'react';
+import { Card } from 'antd';
+import Meta from 'antd/es/card/Meta';
+import './Products.scss';
 
 const Product = (props) => {
-    const printProducts = props.products.map(product => {
-        return <Card
-            key={product.id}
-            hoverable
-            style={{ width: 240 }}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-        >
-            <Meta title={product.name} description={product.price} />
-            <p>description:{product.description}</p>
-            <p>size:{product.size}</p>
-        </Card>
-    })
-
-
-
+  const printProducts = props.products.map(product => {
     return (
-        <div>
-            {printProducts}
-        </div>
-    )
+      <div className="product-card" key={product.id}>
+        <Card
+          hoverable
+          style={{ width: '100%' }}
+          cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+        >
+          <Meta title={product.name} description={`$${product.price}`} />
+          <p>description: {product.description}</p>
+          <p>size: {product.size}</p>
+        </Card>
+      </div>
+    );
+  });
+
+  return (
+    <div className="products-grid">
+      {printProducts}
+    </div>
+  );
 }
 
-export default Product
+export default Product;
