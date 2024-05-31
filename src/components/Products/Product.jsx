29d1e2f706fe1e3eaab3c6825d65188501b516bd
sentobc13 <sentoberlanga@gmail.com>
2024-05-31 11:34:01 +0200

    unsplash api eliminada

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,81 +1,11 @@
-// import React, { useContext } from 'react';
-// import { Card } from 'antd';
-// import Meta from 'antd/es/card/Meta';
-// import './Products.scss';
-// import { ProductsContext } from '../../context/ProductsContext/ProductsState';
-
-// const Product = () => {
-//     const { products, addCart } = useContext(ProductsContext);
-
-//     const printProducts = products.map(product => {
-//         return (
-//             <div className="product-card" key={product.id}>
-//                 <Card
-//                     hoverable
-//                     style={{ width: '100%' }}
-//                     cover={<img alt={product.name} src={product.image || 'https://via.placeholder.com/150'} />}  // Placeholder image if none provided
-//                 >
-//                     <div className="card-info">
-//                         <Meta title={product.name} />
-//                         <p className="text-body">{product.description}</p>
-//                         <p className="text-body">Talla: {product.size}</p>
-//                     </div>
-//                     <div className="card-footer">
-//                         <span className="text-title">€{product.price}</span>
-//                         <div className="card-button" onClick={() => addCart(product)}>
-//                             <svg className="svg-icon" viewBox="0 0 20 20">
-//                                 <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
-//                                 <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
-//                                 <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
-//                             </svg>
-//                         </div>
-//                     </div>
-//                 </Card>
-//             </div>
-//         );
-//     });
-
-//     return (
-//         <div className="products-grid">
-//             {printProducts}
-//         </div>
-//     );
-// }
-
-// export default Product;
-
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { Card } from 'antd';
 import Meta from 'antd/es/card/Meta';
 import './Products.scss';
 import { ProductsContext } from '../../context/ProductsContext/ProductsState';
-import axios from 'axios'; // Importa axios para hacer solicitudes HTTP
 
 const Product = () => {
     const { products, addCart } = useContext(ProductsContext);
-    const [productImages, setProductImages] = useState({}); // Estado para almacenar las imágenes de los productos
-
-    useEffect(() => {
-        // Función para cargar las imágenes de los productos desde Unsplash
-        const fetchProductImages = async () => {
-            try {
-                const imageRequests = products.map(product => (
-                    axios.get(`https://api.unsplash.com/photos/random?query=clothing&client_id=pznjLkVyjt7a3c-3pZUmP2U38bcDaDFnoSWOjqshGTA`)
-                ));
-                const responses = await Promise.all(imageRequests);
-                const productImagesData = responses.map(response => response.data);
-                const images = productImagesData.reduce((acc, cur, idx) => {
-                    acc[products[idx].id] = cur.urls.small;
-                    return acc;
-                }, {});
-                setProductImages(images);
-            } catch (error) {
-                console.error('Error fetching product images:', error);
-            }
-        };
-
-        fetchProductImages(); // Llama a la función para cargar las imágenes al montar el componente
-    }, [products]);
 
     const printProducts = products.map(product => {
         return (
@@ -83,7 +13,7 @@ const Product = () => {
                 <Card
                     hoverable
                     style={{ width: '100%' }}
-                    cover={<img alt={product.name} src={productImages[product.id] || 'https://via.placeholder.com/150'} />}  // Utiliza la imagen cargada desde Unsplash o un marcador de posición si no hay una disponible
+                    cover={<img alt={product.name} src={product.image || 'https://via.placeholder.com/150'} />}  
                 >
                     <div className="card-info">
                         <Meta title={product.name} />
@@ -94,7 +24,9 @@ const Product = () => {
                         <span className="text-title">€{product.price}</span>
                         <div className="card-button" onClick={() => addCart(product)}>
                             <svg className="svg-icon" viewBox="0 0 20 20">
-                                {/* Icono de carrito de compras */}
+                                <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
+                                <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
+                                <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
                             </svg>
                         </div>
                     </div>
@@ -111,4 +43,3 @@ const Product = () => {
 }
 
 export default Product;
-
